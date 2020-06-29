c91faedaf4f29e0a3cc3b21c20f56f3c5e605870
Paige M <65613628+justpaige@users.noreply.github.com>
2020-06-28 20:51:46 -0400

    Update SpeakHello.js

--- coursera-module4/SpeakHello.js
+++ coursera-module4/SpeakHello.js
@@ -1,10 +1,10 @@
 //Module 4 Assignment speakhello.js
 (function(window){
-  var speakWord = "Hello";
+  var speakWord = "Hello ";
   var helloSpeaker = {};
 
   helloSpeaker.speak = function (name) {
-      console.log(speakWord + " " + name);
+      console.log(speakWord + name);
   }
 
   window.helloSpeaker = helloSpeaker;
